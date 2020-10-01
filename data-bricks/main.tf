073a081a69f5074348eeed9d983a56431c1b519b
nicogriffioen <60692751+nicogriffioen@users.noreply.github.com>
2020-10-01 15:56:27 +0200

    Use databricks sku from config. DAT-4315

--- data-bricks/main.tf
+++ data-bricks/main.tf
@@ -17,5 +17,5 @@ resource "azurerm_databricks_workspace" "databricks" {
   name                = var.databricks_name
   resource_group_name = var.resource_group_name
   location            = var.location
-  sku                 = "standard"
+  sku                 = var.databricks_sku
 }
