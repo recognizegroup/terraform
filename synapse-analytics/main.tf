a00148939f237024e0caf22da09eb44769d961ac
j.vanenckevort <j.vanenckevort@vwt.digital>
2020-10-12 14:51:54 +0200

    DAT-4388. Create synapse SQL dwh module

--- synapse-analytics/main.tf
+++ synapse-analytics/main.tf
@@ -7,3 +7,41 @@ terraform {
 
   backend "azurerm" {}
 }
+
+provider "azurerm" {
+  skip_provider_registration = var.skip_provider_registration
+  features {}
+}
+
+data "azurerm_key_vault" "key_vault" {
+  name                = var.key_vault_name
+  resource_group_name = var.resource_group_name
+}
+
+data "azurerm_key_vault_secret" "sql_admin_user_secret" {
+  name         = var.sql_admin_user_secret_name
+  key_vault_id = data.azurerm_key_vault.key_vault.id
+}
+
+data "azurerm_key_vault_secret" "sql_admin_password_secret" {
+  name         = var.sql_admin_password_secret_name
+  key_vault_id = data.azurerm_key_vault.key_vault.id
+}
+
+resource "azurerm_sql_server" "sql_server" {
+  name                             = var.sql_server_name
+  resource_group_name              = var.resource_group_name
+  location                         = var.location
+  version                          = var.sql_server_version
+  administrator_login              = data.azurerm_key_vault_secret.sql_admin_user_secret.value
+  administrator_login_password     = data.azurerm_key_vault_secret.sql_admin_password_secret.value
+}
+
+resource "azurerm_sql_database" "sql_database" {
+  name                             = var.sql_database_name
+  resource_group_name              = var.resource_group_name
+  location                         = var.location
+  server_name                      = azurerm_sql_server.sql_server.name
+  edition                          = var.sql_edition
+  requested_service_objective_name = var.sql_service_level
+}
