198dae6d57f8c121e06763b68d3ee1d24ec331c4
j.vanenckevort <j.vanenckevort@vwt.digital>
2020-10-26 08:42:48 +0100

    DAT-4489. Improve datafactory module

--- modules/data-factory/main.tf
+++ modules/data-factory/main.tf
@@ -13,35 +13,93 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_data_factory" "datafactory" {
+data "azurerm_storage_account" "storage_account" {
+  name                = var.datalake_storage_account_name
+  resource_group_name = var.resource_group_name
+}
+
+resource "azurerm_data_factory" "data_factory" {
+  name                = var.data_factory_name
   resource_group_name = var.resource_group_name
   location            = var.location
-  name                = var.datafactory_name
 }
 
-resource "azurerm_data_factory_linked_service_azure_blob_storage" "example" {
-  name                = "example"
+resource "azurerm_data_factory_linked_service_web" "service_web" {
+  name                = var.service_web_name
   resource_group_name = var.resource_group_name
-  data_factory_name   = var.location
-  connection_string   = data.azurerm_storage_account.example.primary_connection_string
-
+  data_factory_name   = azurerm_data_factory.data_factory.name
+  authentication_type = var.service_web_authentication_type
+  url                 = var.service_web_url
 }
 
-resource "azurerm_data_factory_linked_service_web" "example" {
-  name                = "example"
+resource "azurerm_data_factory_dataset_http" "dataset_web" {
+  name                = var.dataset_web_name
   resource_group_name = var.resource_group_name
-  data_factory_name   = azurerm_data_factory.datafactory.name
-  authentication_type = "Anonymous"
-  url                 = "https://downloads.pdok.nl/kadastralekaart/api/v4_0/dataset"
+  data_factory_name   = azurerm_data_factory.data_factory.name
+  linked_service_name = azurerm_data_factory_linked_service_web.service_web.name
+  relative_url        = var.dataset_web_relative_url
+  request_method      = var.dataset_web_request_method
 }
 
-resource "azurerm_data_factory_dataset_http" "example" {
-  name                = "example"
+resource "azurerm_data_factory_linked_service_azure_blob_storage" "service_blob" {
+  name                = var.service_blob_name
   resource_group_name = var.resource_group_name
-  data_factory_name   = azurerm_data_factory.datafactory.name
-  linked_service_name = azurerm_data_factory_linked_service_web.example.name
+  data_factory_name   = azurerm_data_factory.data_factory.name
+  connection_string   = data.azurerm_storage_account.storage_account.primary_connection_string
+}
 
-  relative_url   = "https://downloads.pdok.nl/kadastralekaart/api/v4_0/dataset"
-  request_method = "GET"
+resource "azurerm_data_factory_dataset_azure_blob" "dataset_blob" {
+  name                = var.dataset_blob_name
+  resource_group_name = var.resource_group_name
+  data_factory_name   = azurerm_data_factory.data_factory.name
+  linked_service_name = azurerm_data_factory_linked_service_azure_blob_storage.service_blob.name
+  path                = var.datalake_storage_container_name
+  filename            = var.dataset_blob_filename
+}
 
+resource "azurerm_data_factory_trigger_schedule" "schedule" {
+  name                = var.data_factory_schedule_name
+  data_factory_name   = azurerm_data_factory.data_factory.name
+  resource_group_name = var.resource_group_name
+  pipeline_name       = azurerm_data_factory_pipeline.pipeline.name
+  interval            = var.schedule_interval
+  frequency           = var.schedule_frequency
+}
+
+resource "azurerm_data_factory_pipeline" "pipeline" {
+  name                = var.data_factory_pipeline_name
+  resource_group_name = var.resource_group_name
+  data_factory_name   = azurerm_data_factory.data_factory.name
+  activities_json     = <<JSON
+[
+  {
+    "name": "CopyData",
+    "type": "Copy",
+    "inputs": [
+      {
+        "referenceName": "${azurerm_data_factory_dataset_http.dataset_web.name}",
+        "type": "DatasetReference"
+      }
+    ],
+    "outputs": [
+      {
+        "referenceName": "${azurerm_data_factory_dataset_azure_blob.dataset_blob.name}",
+        "type": "DatasetReference"
+      }
+    ],
+    "typeProperties": {
+      "source": {
+        "type": "RestSource",
+        "httpRequestTimeout": "00:01:00"
+      },
+      "sink": {
+        "type": "BinarySink",
+        "storeSettings": {
+          "type": "AzureBlobStorageWriteSettings"
+        }
+      }
+    }
+  }
+]
+  JSON
 }
--- modules/data-factory/variables.tf
+++ modules/data-factory/variables.tf
@@ -8,13 +8,93 @@ variable "resource_group_name" {
   description = "Name of the resource group."
 }
 
+variable "datalake_storage_account_name" {
+  type        = string
+  description = "Name of the storage account"
+}
+
+variable "datalake_storage_container_name" {
+  type        = string
+  description = "Name of the storage container."
+}
+
 variable "skip_provider_registration" {
   type        = bool
   description = "Indicates whether the provider should be registered in Azure."
   default     = false
 }
 
-variable "datafactory_name" {
+variable "data_factory_name" {
   type        = string
   description = "The name of the datafactory."
+}
+
+variable "data_factory_pipeline_name" {
+  type        = string
+  description = "The name of the data factory pipeline."
+}
+
+variable "service_web_name" {
+  type        = string
+  description = "The name of the data factory web service."
+}
+
+variable "service_web_authentication_type" {
+  type        = string
+  description = "The authentication type of the data factory web service."
+  default     = "Anonymous"
+}
+
+variable "service_web_url" {
+  type        = string
+  description = "The url of the data factory web service."
+  default     = "Anonymous"
+}
+
+variable "dataset_web_name" {
+  type        = string
+  description = "The name of the data factory http dataset."
+}
+
+variable "dataset_web_relative_url" {
+  type        = string
+  description = "The relative url of the data factory http dataset."
+}
+
+variable "dataset_web_request_method" {
+  type        = string
+  description = "The request method of the data factory http dataset."
+  default     = "Get"
+}
+
+variable "service_blob_name" {
+  type        = string
+  description = "The name of the data factory blob service."
+}
+
+variable "dataset_blob_name" {
+  type        = string
+  description = "The name of the data factory blob dataset."
+}
+
+variable "dataset_blob_filename" {
+  type        = string
+  description = "The filename of the data factory blob dataset."
+}
+
+variable "data_factory_schedule_name" {
+  type        = string
+  description = "The name of the data factory schedule."
+}
+
+variable "schedule_interval" {
+  type        = number
+  description = "The interval of the data factory schedule."
+  default     = 1
+}
+
+variable "schedule_frequency" {
+  type        = string
+  description = "The frequency of the data factory schedule."
+  default     = "Day"
 }
