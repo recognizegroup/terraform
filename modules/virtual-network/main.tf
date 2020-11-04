4852401c0ab4ce15945407422bed220e605e3db5
nicogriffioen <60692751+nicogriffioen@users.noreply.github.com>
2020-11-04 16:12:53 +0100

    Add initial virtual network setup. DAT-4622

---
+++ modules/virtual-network/main.tf
@@ -0,0 +1,34 @@
+terraform {
+  required_version = ">=0.13.4"
+
+  required_providers {
+    azurerm = ">=2.24.0"
+  }
+
+  backend "azurerm" {}
+}
+
+provider "azurerm" {
+  skip_provider_registration = var.skip_provider_registration
+  features {}
+}
+
+resource "azurerm_virtual_network" "virtual_network" {
+
+  name                = var.virtual_network_name
+  address_space       = var.virtual_network_address_space
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  dynamic "subnet" {
+    for_each = [for s in var.virtual_network_subnets : {
+      name   = s.name
+      prefix = s.address_prefix
+    }]
+
+    content {
+      name           = subnet.value.name
+      address_prefix = subnet.value.prefix
+    }
+  }
+}
