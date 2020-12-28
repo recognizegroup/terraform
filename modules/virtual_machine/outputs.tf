4b1ab9ac16da9064202ea07ac977185208910232
j.vanenckevort <j.vanenckevort@vwt.digital>
2020-12-28 14:52:21 +0100

    DAT-4423. Add recovery services for VM backup

--- modules/virtual_machine/outputs.tf
+++ modules/virtual_machine/outputs.tf
@@ -1,3 +1,7 @@
 output "virtual_machine_name" {
   value = azurerm_windows_virtual_machine.virtual_machine.name
 }
+
+output "virtual_machine_id" {
+  value = azurerm_windows_virtual_machine.virtual_machine.id
+}
