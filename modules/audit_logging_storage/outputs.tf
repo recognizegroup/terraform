44b5a5057a95f5232ef2fed31a7c64dc0a8bf69c
j.vanenckevort <j.vanenckevort@vwt.digital>
2020-12-31 12:08:05 +0100

    DAT-5245. Create audit logging storage module

---
+++ modules/audit_logging_storage/outputs.tf
@@ -0,0 +1,12 @@
+output "storage_account_name" {
+  value = azurerm_storage_account.storage_account.name
+}
+
+output "primary_blob_endpoint" {
+  value = azurerm_storage_account.storage_account.primary_blob_endpoint
+}
+
+output "primary_access_key" {
+  value     = azurerm_storage_account.storage_account.primary_access_key
+  sensitive = true
+}
