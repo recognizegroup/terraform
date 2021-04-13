8d8e0478b050d16b1f32f5ee771ea8937ade340e
j.vanenckevort <j.vanenckevort@vwt.digital>
2021-04-13 16:33:24 +0200

    DAT-6143. Add module for analysis services

---
+++ modules/analysis_services/main.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_version = ">=0.14.0"
+
+  required_providers {
+    azurerm = "=2.51.0"
+  }
+
+  backend "azurerm" {}
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_analysis_services_server" "server" {
+  name                    = var.analysis_services_server_full_name
+  location                = var.location
+  resource_group_name     = var.resource_group_name
+  sku                     = var.analysis_services_server_sku
+  admin_users             = var.analysis_services_server_admin_users
+  enable_power_bi_service = var.analysis_services_server_enable_power_bi
+
+
+  dynamic "ipv4_firewall_rule" {
+    for_each = toset(var.ipv4_firewall_rules)
+    content {
+      name        = ipv4_firewall_rule.value.name
+      range_start = ipv4_firewall_rule.value.range_start
+      range_end   = ipv4_firewall_rule.value.range_end
+    }
+  }
+}
---
+++ modules/analysis_services/outputs.tf
@@ -0,0 +1,3 @@
+output "analysis_services_server_full_name" {
+  value = azurerm_analysis_services_server.server.server_full_name
+}
