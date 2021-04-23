591ccc2ca72dc0eb98936b731fe91e10c54c711e
Jeroen Vleeming <j.vleeming@recognize.nl>
2021-04-23 08:55:57 +0200

    Added ASP

---
+++ modules/app-service-plan/vars.tf
@@ -0,0 +1,24 @@
+variable "location" {
+  type        = string
+  description = "A datacenter location in Azure."
+}
+
+variable "resource_group_name" {
+  type        = string
+  description = "Name of the resource group."
+}
+
+variable "app_service_plan_name" {
+  type = string
+  description = "Name of the ASP"
+}
+
+variable "app_service_plan_tier" {
+  type = string
+  description = "Tier of the ASP"
+}
+
+variable "app_service_plan_size" {
+  type = string
+  description = "size of the ASP"
+}
