8ab3a916e6acd96f03981fc62f5e9effa959cb94
j.vanenckevort <j.vanenckevort@vwt.digital>
2021-06-15 08:59:31 +0200

    VWNP-9. App service plan default windows environment

--- modules/app_service_plan/main.tf
+++ modules/app_service_plan/main.tf
@@ -16,6 +16,7 @@ resource "azurerm_app_service_plan" "asp" {
   name                = var.app_service_plan_name
   location            = var.location
   resource_group_name = var.resource_group_name
+  kind                = var.app_service_plan_kind
 
   sku {
     tier = var.app_service_plan_tier
