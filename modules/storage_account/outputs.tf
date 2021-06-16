98f0003a912299cf7288d00c6a60946761eea4f0
Jeroen Vleeming <j.vleeming@recognize.nl>
2021-06-16 16:07:01 +0200

    RPAAR-12 Added storage account and an option to create an blob container as well

---
+++ modules/storage_account/outputs.tf
@@ -0,0 +1,17 @@
+output "storage_account_name" {
+  value = azurerm_storage_account.storage_account.name
+}
+
+output "primary_blob_endpoint" {
+  value = azurerm_storage_account.storage_account.primary_blob_endpoint
+}
+
+output "primary_access_key" {
+  value     = azurerm_storage_account.storage_account.primary_access_key
+  sensitive = true
+}
+
+output "primary_access_connection_string" {
+  value     = azurerm_storage_account.storage_account.primary_connection_string
+  sensitive = true
+}
