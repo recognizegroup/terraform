96285880338c83dd372e9cb0efe769d69783c858
Joep Kemperman <j.kemperman@recognize.nl>
2021-08-11 13:13:49 +0200

    SA-47 Add public app service module

---
+++ modules/app_service_public/main.tf
@@ -0,0 +1,71 @@
+terraform {
+  required_version = ">=0.14.9"
+
+  required_providers {
+    azurerm = "=2.66.0"
+  }
+
+  backend "azurerm" {}
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_app_service" "app_service" {
+  name                = var.app_service_name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  app_service_plan_id = var.app_service_plan_id
+  https_only          = true
+
+  site_config {
+    scm_type                 = var.scm_type
+    always_on                = true
+    ftps_state               = "AllAllowed"
+    dotnet_framework_version = var.dotnet_framework_version
+    websockets_enabled       = var.websockets_enabled
+    linux_fx_version         = var.linux_fx_version
+    min_tls_version          = var.min_tls_version
+    health_check_path        = var.health_check_path
+  }
+
+  app_settings = var.app_settings
+
+  connection_string = var.connection_string
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
+
+resource "azurerm_app_service_virtual_network_swift_connection" "vnet_integration" {
+  count          = var.integration_subnet_id == null ? 0 : 1
+  app_service_id = azurerm_app_service.app_service.id
+  subnet_id      = var.integration_subnet_id
+}
+
+resource "azurerm_private_endpoint" "private_endpoint" {
+  name                = var.private_endpoint_name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  subnet_id           = var.private_subnet_id
+
+  private_service_connection {
+    name                           = var.private_service_connection_name
+    is_manual_connection           = false
+    private_connection_resource_id = azurerm_app_service.app_service.id
+    subresource_names              = ["sites"]
+  }
+
+  lifecycle {
+    ignore_changes = [private_dns_zone_group]
+  }
+}
+
+resource "azurerm_app_service_custom_hostname_binding" "custom_domain" {
+  for_each            = toset(var.custom_domains)
+  hostname            = each.value
+  app_service_name    = azurerm_app_service.app_service.name
+  resource_group_name = var.resource_group_name
+}
---
+++ modules/app_service_public/variables.tf
@@ -0,0 +1,82 @@
+variable "location" {
+  type        = string
+  description = "A datacenter location in Azure."
+}
+
+variable "resource_group_name" {
+  type        = string
+  description = "Name of the resource group."
+}
+
+variable "app_service_name" {
+  type        = string
+  description = "Name of the app service."
+}
+
+variable "app_service_plan_id" {
+  type        = string
+  description = "ID of the app service plan."
+}
+
+variable "scm_type" {
+  type        = string
+  description = "The type of Source Control enabled for this App Service"
+}
+
+variable "app_settings" {
+  type        = map
+  description = "The app settings of this app service."
+  default     = {}
+}
+
+variable "connection_string" {
+  type = object({
+    name  = string,
+    type  = string,
+    value = string
+  })
+  description = "A connection string used by the app service."
+  default     = null
+}
+
+variable "linux_fx_version" {
+  type        = string
+  description = "Linux app framework and version for the app service."
+  default     = null
+}
+
+variable "dotnet_framework_version" {
+  type        = string
+  description = "The .NET framework version of this app service."
+  default     = "v5.0"
+}
+
+variable "websockets_enabled" {
+  type        = bool
+  description = "Indicates whether websockets are enabled within this app service."
+  default     = true
+}
+
+variable "min_tls_version" {
+  type        = string
+  description = "The minimum supported TLS version for the app service."
+  default     = "1.2"
+}
+
+variable "health_check_path" {
+  type        = string
+  description = "The health check path to be pinged by App Service."
+  default     = null
+}
+
+variable "integration_subnet_id" {
+  type        = string
+  description = "The ID of the Subnet from which private IP addresses will be allocated for this Private Endpoint."
+  default     = null
+}
+
+variable "custom_domains" {
+  type        = list
+  description = "Define custom domains attached to this app service."
+  default     = []
+}
