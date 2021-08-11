1a1b218d70b5fbe81f2b5d19386a3e4899040067
Joep Kemperman <j.kemperman@recognize.nl>
2021-08-11 14:15:00 +0200

    Remove private endpoitn from public app service

--- modules/app_service_public/main.tf
+++ modules/app_service_public/main.tf
@@ -45,24 +45,6 @@ resource "azurerm_app_service_virtual_network_swift_connection" "vnet_integratio
   subnet_id      = var.integration_subnet_id
 }
 
-resource "azurerm_private_endpoint" "private_endpoint" {
-  name                = var.private_endpoint_name
-  location            = var.location
-  resource_group_name = var.resource_group_name
-  subnet_id           = var.private_subnet_id
-
-  private_service_connection {
-    name                           = var.private_service_connection_name
-    is_manual_connection           = false
-    private_connection_resource_id = azurerm_app_service.app_service.id
-    subresource_names              = ["sites"]
-  }
-
-  lifecycle {
-    ignore_changes = [private_dns_zone_group]
-  }
-}
-
 resource "azurerm_app_service_custom_hostname_binding" "custom_domain" {
   for_each            = toset(var.custom_domains)
   hostname            = each.value
