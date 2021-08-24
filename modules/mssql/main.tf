6121a1eb22fb4cd278ead945149876f1914b8495
Jeroen Vleeming <j.vleeming@recognize.nl>
2021-08-24 11:45:05 +0200

    SA-69 added option to use own username and random password when deploying mssql in stead of using secrets

--- modules/mssql/main.tf
+++ modules/mssql/main.tf
@@ -29,13 +29,17 @@ data "azurerm_key_vault_secret" "sql_admin_password_secret" {
   key_vault_id = data.azurerm_key_vault.key_vault.id
 }
 
+resource "random_password" "password" {
+  length = 32
+  special = false
+}
 resource "azurerm_mssql_server" "sql_server" {
   name                          = var.sql_server_name
   resource_group_name           = var.resource_group_name
   location                      = var.location
   version                       = var.sql_server_version
-  administrator_login           = data.azurerm_key_vault_secret.sql_admin_user_secret.value
-  administrator_login_password  = data.azurerm_key_vault_secret.sql_admin_password_secret.value
+  administrator_login           = var.sql_admin_user_name != null? var.sql_admin_user_name: data.azurerm_key_vault_secret.sql_admin_user_secret.value
+  administrator_login_password  = var.use_random_password ? random_password.password.result: data.azurerm_key_vault_secret.sql_admin_password_secret.value
   public_network_access_enabled = var.public_network_access_enabled
 
   lifecycle {
