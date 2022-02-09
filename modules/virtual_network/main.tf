046e19e147a29a144fdf865bd671c1bc95fa8053
j.vanenckevort <j.vanenckevort@vwt.digital>
2022-02-09 17:28:28 +0100

    FIB-402. Ignore changes subnets in vnet

--- modules/virtual_network/main.tf
+++ modules/virtual_network/main.tf
@@ -17,4 +17,9 @@ resource "azurerm_virtual_network" "virtual_network" {
   address_space       = var.address_space
   location            = var.location
   resource_group_name = var.resource_group_name
+
+  # Deployed as a separate resource
+  lifecycle {
+    ignore_changes = [subnet]
+  }
 }
