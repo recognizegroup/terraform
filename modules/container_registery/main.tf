493f764dc24d4f217b1fbe8606b73a5cc5588799
Artiom Matievschi <Artiom.Matievschi@gmail.com>
2022-03-01 09:50:58 +0100

    Ceate First iterration of the Container registery with default values

---
+++ modules/container_registery/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_version = ">=1.1.2"
+
+  required_providers {
+    azurerm = "=2.94.0"
+  }
+
+  backend "azurerm" {}
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_container_registry" "acr" {
+  name                = var.name
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  sku                 = var.sku
+}
---
+++ modules/container_registery/variables.tf
@@ -0,0 +1,19 @@
+variable "location" {
+  type        = string
+  description = "A datacenter location in Azure."
+}
+
+variable "resource_group_name" {
+  type        = string
+  description = "Name of the resource group."
+}
+
+variable "name" {
+  type        = string
+  description = "Specifies the name of the application insights component."
+}
+
+variable "sku" {
+  type    = string
+  default = "Standard"
+}
