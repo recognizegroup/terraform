ccb0cc5595fbc72ba78fd354a70977c496bbbc5c
ArtiomMatiom <a.matievschi@recognize.nl>
2022-03-31 12:09:28 +0200

    Expose Primary access Connection string varaible within the module

--- modules/storage_account_private/outputs.tf
+++ modules/storage_account_private/outputs.tf
@@ -14,3 +14,8 @@ output "primary_access_key" {
   value     = azurerm_storage_account.storage_account.primary_access_key
   sensitive = true
 }
+
+output "primary_access_connection_string" {
+  value     = azurerm_storage_account.storage_account.primary_connection_string
+  sensitive = true
+}
