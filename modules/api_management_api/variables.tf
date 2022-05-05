268399529304fdccf30ec3e7f4a0bf4badd63cc8
Justin-Booij <101110100+Justin-Booij@users.noreply.github.com>
2022-05-05 11:32:47 +0200

    7903; apim_api module to single openapi spec (#121)
    7903; api_management_api module now takes a single openAPI file path rather then a list of files in root

--- modules/api_management_api/variables.tf
+++ modules/api_management_api/variables.tf
@@ -8,7 +8,12 @@ variable "api_management_name" {
   description = "The name of the API management service."
 }
 
-variable "api_management_group_name" {
+# variable "api_management_group_name" {
+#   type        = string
+#   description = "The name of the API management group."
+# }
+
+variable "openapi_file_path"{
   type        = string
-  description = "The name of the API management group."
+  description = "Path to an openAPI .yaml file that describes this API"
 }
