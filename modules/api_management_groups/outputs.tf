3c8972438c344a7f8c235c01ba7b2f955a7bbdc9
Justin-Booij <j.booij@recognize.nl>
2022-05-09 11:33:15 +0200

    removed user/user_groups for AAD groups

---
+++ modules/api_management_groups/outputs.tf
@@ -0,0 +1,5 @@
+output "group_names" {
+    value = [
+        for group in azurerm_api_management_group.group : group.name
+    ]
+}
---
+++ modules/api_management_groups/variables.tf
@@ -0,0 +1,19 @@
+variable "resource_group_name" {
+  type        = string
+  description = "Name of the resource group."
+}
+
+variable "api_management_name" {
+  type        = string
+  description = "The name of the API management service."
+}
+
+
+variable "groups" {
+    type      = list(object({
+        name          = string
+        display_name  = string
+        external_id   = string
+    }))
+    description = "A list of groups"
+}
