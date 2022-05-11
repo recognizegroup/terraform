586e3c61a0ecdb26738b70d1f20fdc0697ce4357
Justin-Booij <j.booij@recognize.nl>
2022-05-11 12:05:47 +0200

    added default vars according to story
    default_message_ttl: "P14D",
    max_size_in_megabytes: 1024,
    requires_duplicate_detection: false,
    duplicate_detection_history_time_window: "PT10M",
    enable_batched_operations: true,
    status: "Active",
    support_ordering: true,
    auto_delete_on_idle: "P10675199DT2H48M5.4775807S"
    enable_partitioning: false,
    enable_express: false

--- modules/service_bus_topic/main.tf
+++ modules/service_bus_topic/main.tf
@@ -13,10 +13,16 @@ provider "azurerm" {
 }
 
 resource "azurerm_servicebus_topic" "service_bus_topic" {
-  name                          = var.name
-  namespace_id                  = var.namespace_id
-  default_message_ttl           = var.default_message_ttl
-  max_size_in_megabytes         = var.max_size_in_megabytes
-  enable_partitioning           = var.enable_partitioning
-  requires_duplicate_detection  = var.requires_duplicate_detection
+  name                                    = var.name
+  namespace_id                            = var.namespace_id
+  default_message_ttl                     = var.default_message_ttl
+  max_size_in_megabytes                   = var.max_size_in_megabytes
+  enable_partitioning                     = var.enable_partitioning
+  requires_duplicate_detection            = var.requires_duplicate_detection
+  duplicate_detection_history_time_window = var.duplicate_detection_history_time_window
+  enable_batched_operations               = var.enable_batched_operations
+  status                                  = var.status
+  support_ordering                        = var.support_ordering
+  auto_delete_on_idle                     = var.auto_delete_on_idle
+  enable_express                          = var.enable_express
 }
