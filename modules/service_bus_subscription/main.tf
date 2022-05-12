a91ed804eadd8544816b4dd553426a69ecbc95b9
Rick van Sloten <r.vansloten@recognize.nl>
2022-05-12 16:35:01 +0200

    7983 - Set new variables

--- modules/service_bus_subscription/main.tf
+++ modules/service_bus_subscription/main.tf
@@ -13,7 +13,14 @@ provider "azurerm" {
 }
 
 resource "azurerm_servicebus_subscription" "service_bus_subscription" {
-  name                          = var.name
-  topic_id                      = var.topic_id
-  max_delivery_count            = var.max_delivery_count
+  name                                      = var.name
+  topic_id                                  = var.topic_id
+  max_delivery_count                        = var.max_delivery_count
+  lock_duration                             = var.lock_duration
+  requires_session                          = var.requires_session
+  default_message_ttl                       = var.default_message_ttl
+  dead_lettering_on_message_expiration      = var.dead_lettering_on_message_expiration
+  dead_lettering_on_filter_evaluation_error = var.dead_lettering_on_filter_evaluation_error
+  enable_batched_operations                 = var.enable_batched_operations
+  auto_delete_on_idle                       = var.auto_delete_on_idle
 }
