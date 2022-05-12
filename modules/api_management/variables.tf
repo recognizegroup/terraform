5546324291ca2853d1275abe43791cf62b00ac26
ArtiomMatiom <a.matievschi@recognize.nl>
2022-05-12 15:01:27 +0200

    Remove Extra line

--- modules/api_management/variables.tf
+++ modules/api_management/variables.tf
@@ -63,7 +63,6 @@ variable "api_management_logger_settings" {
   default     = null
 }
 
-
 variable "azurerm_api_management_diagnostic_settings" {
   type = object({
     sampling_percentage       = number,
