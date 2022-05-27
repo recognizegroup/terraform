747f0aa378cee50ceb72d61d124f9f1bebae2b4d
Justin Booij <101110100+Justin-Booij@users.noreply.github.com>
2022-05-27 16:25:42 +0200

    added aad authorization for api's (#141)
    * added aad authorization for api's
    
    * whiteline
    
    * Update variables.tf

--- modules/api_management_api/variables.tf
+++ modules/api_management_api/variables.tf
@@ -18,6 +18,23 @@ variable "openapi_file_path" {
   description = "Path to an openAPI .yaml file that describes this API"
 }
 
+variable "owners" {
+  type        = list(any)
+  description = "List of AAD object IDs to set as API management owners."
+  default     = []
+}
+
+variable "object_ids" {
+  type        = list(string)
+  description = "managed identity object ids that want access to the through AAD application"
+}
+
+variable "require_api_subscription" {
+  type        = string
+  description = "bool value for required subscriptions at the api level"
+  default     = false
+}
+
 variable "api_management_logger_id" {
   type        = string
   description = "The Id of the API management Logger"
