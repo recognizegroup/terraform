be449f4ea4fd8fbd258542029f98c2507495706b
Jeffrey Verpoort <j.verpoort@vwtelecom.com>
2022-06-20 05:27:21 +0200

    Remove data modules, they will be in every repository

--- data_modules/log_analytics_workspace/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "log_analytics_workspace_name" {
-  type        = string
-  description = "The log analytics workspace name"
-}
-
-variable "resource_group_name" {
-  type        = string
-  description = "The name of the resource group the log analytics is in"
-}
