c9ef1732964b6aba55d21386fdcedcb985cd2142
j.vanenckevort <j.vanenckevort@vwt.digital>
2022-06-21 12:09:53 +0200

    8700. Refactored policy_xml in apim so that manipulations can be done more easily inside terragrunt

--- modules/api_management_api/main.tf
+++ modules/api_management_api/main.tf
@@ -14,6 +14,8 @@ terraform {
 
   backend "azurerm" {}
 
+  # Optional attributes and the defaults function are
+  # both experimental, so we must opt in to the experiment.
   experiments = [module_variable_optional_attrs]
 }
 
