a5efcb5f99517bbaf93fcaf24db64eec130e7793
Justin-Booij <j.booij@recognize.nl>
2022-06-21 09:41:14 +0200

    removed resource name as an option

--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -12,17 +12,12 @@ provider "azurerm" {
   features {}
 }
 
-data "azurerm_resource_group" "resource_group" {
-  count = var.resource_group_name != null ? 1 : 0
-  name  = var.resource_group_name
-}
-
 resource "azurerm_role_assignment" "role_assignment" {
   for_each = {
     for role in var.roles :
     "${role.object_id}_${role.role_name}" => role
   }
-  scope                = var.scope != null ? var.scope : data.azurerm_resource_group.resource_group[0].id
+  scope                = var.scope
   role_definition_name = each.value.role_name
   principal_id         = each.value.object_id
 }
