6b8269ed6b7a18428db06e1a134041ad9a187e5b
Justin Booij <101110100+Justin-Booij@users.noreply.github.com>
2022-07-07 11:14:10 +0200

    added tags (#164)

--- modules/resource_group/variables.tf
+++ modules/resource_group/variables.tf
@@ -7,3 +7,9 @@ variable "name" {
   type        = string
   description = "Name of the resource group."
 }
+
+variable "tags" {
+  type        = map(any)
+  description = "Optional tags for the resource group."
+  default     = null
+}
