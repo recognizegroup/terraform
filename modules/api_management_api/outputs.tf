7f06e7da3cc2afcdc77e815f951e0ddbe9f9f1f6
Justin-Booij <j.booij@recognize.nl>
2022-07-08 13:38:33 +0200

    add application_id to api outputs

--- modules/api_management_api/outputs.tf
+++ modules/api_management_api/outputs.tf
@@ -5,3 +5,7 @@ output "api_name" {
 output "api_management_api_diagnostic" {
   value = (var.api_diagnostic_settings != null && var.api_management_logger_id != null) ? azurerm_api_management_api_diagnostic.api_diagnostic[0].id : null
 }
+
+output "application_id" {
+  value = azuread_application.application.application_id
+}
