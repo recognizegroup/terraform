ec4c17494185c54f36aed6e7956ea7f6c1bc8c55
Justin-Booij <j.booij@recognize.nl>
2022-07-12 12:47:48 +0200

    Created application_role_assignment module

---
+++ modules/application_role_assignment/main.tf
@@ -0,0 +1,34 @@
+terraform {
+  required_version = ">=1.0.9"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.6.0"
+    }
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "=2.22.0"
+    }
+  }
+
+  backend "azurerm" {}
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azuread_service_principal" "internal" {
+  application_id = var.application_id
+}
+
+resource "azuread_app_role_assignment" "role_assignment" {
+  for_each = {
+    for assignment in var.assignments :
+    "${assignment.role_id}_${assignment.object_id}" => assignment
+  }
+  app_role_id         = each.value.role_id
+  principal_object_id = each.value.object_id
+  resource_object_id  = azuread_service_principal.internal.object_id
+}
