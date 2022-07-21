68381f7f61c1f593b125b50d7c59e942e0a96c72
ArtiomMatiom <Artiom.Matievschi@gmail.com>
2022-07-21 13:40:54 +0200

    API management Policy -> policy not active yet

---
+++ modules/api_management_api_operation_policy/main.tf
@@ -0,0 +1,39 @@
+terraform {
+  required_version = ">=1.0.9"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.6.0"
+    }
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "=2.22.0"
+    }
+  }
+
+  backend "azurerm" {}
+
+  # Optional attributes and the defaults function are
+  # both experimental, so we must opt in to the experiment.
+  experiments = [module_variable_optional_attrs]
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_api_management_api_operation_policy" "policy" {
+  api_name            = var.api_name
+  api_management_name = var.api_management_name
+  resource_group_name = var.resource_group_name
+
+  operation_id = var.operation_id
+  xml_content  = <<XML
+  <policies>
+    <inbound>
+      <find-and-replace from="xyz" to="abc" />
+    </inbound>
+  </policies>
+  XML
+}
---
+++ modules/api_management_api_operation_policy/variables.tf
@@ -0,0 +1,19 @@
+variable "resource_group_name" {
+  type        = string
+  description = "Name of the resource group."
+}
+
+variable "operation_id" {
+  type        = string
+  description = "id of the operation"
+}
+
+variable "api_management_name" {
+  type        = string
+  description = "The name of the API management service."
+}
+
+variable "api_name" {
+  type        = string
+  description = "API name"
+}
