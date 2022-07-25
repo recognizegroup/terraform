765186525534648320e54d23f14144470509bf0f
ArtiomMatiom <Artiom.Matievschi@gmail.com>
2022-07-25 10:13:35 +0200

    remove Redundant Provider

--- modules/api_management_api_operation_policy/main.tf
+++ modules/api_management_api_operation_policy/main.tf
@@ -6,10 +6,6 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "=3.6.0"
     }
-    azuread = {
-      source  = "hashicorp/azuread"
-      version = "=2.22.0"
-    }
   }
 
   backend "azurerm" {}
