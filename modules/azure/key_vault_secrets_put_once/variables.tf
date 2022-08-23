2639277beb542a0506ee3afd465994e62ce21792
Jeffrey Verpoort <j.verpoort@vwtelecom.com>
2022-08-23 13:21:19 +0200

    Add KeyVautl secret once to allow developers to rotate.

---
+++ modules/azure/key_vault_secrets_put_once/variables.tf
@@ -0,0 +1,13 @@
+variable "secrets" {
+  type = list(object({
+    secret_name  = string
+    secret_value = string
+  }))
+  description = "A list containing secret objects."
+  sensitive   = true
+}
+
+variable "key_vault_id" {
+  type        = string
+  description = "ID of the key vault."
+}
