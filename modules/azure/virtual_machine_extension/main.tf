9b5fef5661a56b72af92fff640fcb8f9d01ca347
ArtiomMatiom <Artiom.Matievschi@gmail.com>
2022-09-12 12:09:41 +0200

    Posibility to Create Extension for the VM

--- modules/azure/virtual_machine_extension/main.tf
+++ modules/azure/virtual_machine_extension/main.tf
@@ -13,18 +13,21 @@ provider "azurerm" {
 }
 
 resource "azurerm_virtual_machine_extension" "example" {
-  name                 = "hostname"
-  virtual_machine_id   =  var.virtual_machine_id
+  count = var.command_to_execute == null ? 0 : 1
+
+  name                 = var.name
+  virtual_machine_id   = var.virtual_machine_id
   publisher            = "Microsoft.Compute"
   type                 = "CustomScriptExtension"
   type_handler_version = "1.10"
 
-   protected_settings = <<PROTECTED_SETTINGS
+  protected_settings = <<PROTECTED_SETTINGS
     {
-      "commandToExecute": "powershell.exe -File ./fileShare.ps1 -ExecutionPolicy Bypass -fileShareName 4ps-payment-files -storageAccountName stvwtipp2pdev -storageAccountKeys uipLRpBvYH+COWFiWGU6uznjjIGjLiZVa8TmdJQGIzT0mC2GZdFOlHB3eeNSyo4krlIrEv98C6jq+AStNnWSGQ==",
-      "storageAccountName": "stvwtipp2pdev",
-      "storageAccountKey": "uipLRpBvYH+COWFiWGU6uznjjIGjLiZVa8TmdJQGIzT0mC2GZdFOlHB3eeNSyo4krlIrEv98C6jq+AStNnWSGQ==",
-      "fileUris": [${ join(",",var.list_of_file_uris)}]
+      "commandToExecute": "${var.command_to_execute}",
+      "storageAccountName": "${var.storage_account_name}",
+      "storageAccountKey": "${var.storage_account_key}",
+      "fileUris": [
+        "${join("\",\"", var.list_of_file_uris)}"]
     }
   PROTECTED_SETTINGS
 }
--- modules/azure/virtual_machine_extension/variables.tf
+++ modules/azure/virtual_machine_extension/variables.tf
@@ -3,6 +3,10 @@ variable "location" {
   description = "A datacenter location in Azure."
 }
 
+variable "name"{
+  type = string
+  description = "extension name"
+}
 
 variable "virtual_machine_id" {
   type = string
@@ -10,6 +14,22 @@ variable "virtual_machine_id" {
 }
 
 variable "list_of_file_uris" {
-    type = list(string)
-    description = "uris for all the file that have to be downloaded"
+  type = list(string)
+  description = "uris for all the file that have to be downloaded"
+  default = null
+}
+
+variable "storage_account_name" {
+  type = string
+  description = "storage account name containing files"
+}
+variable "storage_account_key" {
+  type = string
+  description = "the access key to the storage account containing required files"
+}
+
+variable "command_to_execute"{
+  type = string
+  description = "cli command to execute"
+  default = null
 }
