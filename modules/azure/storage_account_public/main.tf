2f528acd3a10e9864014161e12e897b499fd6d17
ArtiomMatiom <89966532+ArtiomMatiom@users.noreply.github.com>
2022-10-04 10:01:35 +0200

    Feature/10508 storage account module with nfsv3 (#208)
    Configure ACE setting on NFSv3 Container
    Add NFSv3 Storage account possibility

--- modules/azure/storage_account_public/main.tf
+++ modules/azure/storage_account_public/main.tf
@@ -25,10 +25,25 @@ resource "azurerm_storage_account" "storage_account" {
   nfsv3_enabled             = var.nfsv3_enabled
   is_hns_enabled            = var.is_hns_enabled
 
+  dynamic "network_rules" {
+    for_each = var.nfsv3_enabled == true ? [1] : []
+    content {
+      default_action = "Deny"
+      bypass         = ["AzureServices"]
+      ip_rules       = ["${chomp(data.http.amazonaws[0].body)}"]
+    }
+  }
+
   dynamic "azure_files_authentication" {
     for_each = var.authentication_directory_type == null ? [] : [1]
     content {
-      directory_type           = var.authentication_directory_type
+      directory_type = var.authentication_directory_type
     }
   }
+}
+
+# Get our public IP address
+data "http" "amazonaws" {
+  count = var.nfsv3_enabled == true ? 1 : 0
+  url   = "https://checkip.amazonaws.com/"
 }
