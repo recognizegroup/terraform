dfed811447bc3c2f620f0261d0b262fe5b611193
Justin Booij <101110100+Justin-Booij@users.noreply.github.com>
2022-10-26 08:58:28 +0200

    Feature/10731 function app identity and split windows & linux apps (#222)
    * added function_app_managed_identity
    
    * Split Windows and Linux function app. Settings are different (e.g. Linux has Docker support) and upgrade from existing function_app module is now without modification (only module rename)
    
    * Split Windows and Linux function app. Settings are different (e.g. Linux has Docker support) and upgrade from existing function_app module is now without modification (only module rename)
    
    * Rename function_app to linux_app
    
    * Split Windows and Linux function apps
    
    Co-authored-by: Jeffrey Verpoort <j.verpoort@vwtelecom.com>

---
+++ modules/azure/function_app_linux_managed_identity/main.tf
@@ -0,0 +1,166 @@
+terraform {
+  required_version = ">=0.14.9"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.26.0"
+    }
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "=2.29.0"
+    }
+  }
+
+  backend "azurerm" {}
+}
+
+provider "azurerm" {
+  features {}
+}
+
+locals {
+  should_create_app = var.managed_identity_provider.existing != null ? false : true
+}
+
+# Function App
+
+resource "azurerm_linux_function_app" "function_app" {
+  name                        = var.name
+  location                    = var.location
+  resource_group_name         = var.resource_group_name
+  service_plan_id             = var.app_service_plan_id
+  storage_account_name        = var.storage_account_name
+  storage_account_access_key  = var.storage_account_access_key
+  functions_extension_version = var.runtime_version
+
+  app_settings = var.app_settings
+
+  site_config {
+    always_on              = var.always_on
+    vnet_route_all_enabled = var.route_all_outbound_traffic
+    ip_restriction         = var.ip_restriction
+  }
+
+  auth_settings {
+    enabled = true
+    active_directory {
+      client_id         = local.should_create_app ? azuread_application.application[0].application_id : var.managed_identity_provider.existing.client_id
+      client_secret     = local.should_create_app ? azuread_application_password.password[0].value : var.managed_identity_provider.existing.client_secret
+      allowed_audiences = var.managed_identity_provider.allowed_clients
+    }
+  }
+
+  dynamic "connection_string" {
+    for_each = var.connection_strings
+    content {
+      name  = connection_string.value.name
+      type  = connection_string.value.type
+      value = connection_string.value.value
+    }
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
+
+# Managed Identity Provider
+
+data "azuread_client_config" "current" {}
+
+resource "azuread_application" "application" {
+  count            = local.should_create_app ? 1 : 0
+  display_name     = var.managed_identity_provider.create.display_name
+  owners           = var.managed_identity_provider.create.owners != null ? concat([data.azuread_client_config.current.object_id], var.managed_identity_provider.create.owners) : [data.azuread_client_config.current.object_id]
+  sign_in_audience = "AzureADMyOrg"
+  identifier_uris  = var.managed_identity_provider.create.identifies_uris != null ? var.managed_identity_provider.create.identifies_uris : ["api://${var.managed_identity_provider.create.application_name}"]
+
+  api {
+    requested_access_token_version = 2
+
+    oauth2_permission_scope {
+      admin_consent_description  = var.managed_identity_provider.create.oauth2_settings.admin_consent_description
+      admin_consent_display_name = var.managed_identity_provider.create.oauth2_settings.admin_consent_display_name
+      enabled                    = var.managed_identity_provider.create.oauth2_settings.enabled
+      id                         = random_uuid.oath2_uuid.result
+      type                       = var.managed_identity_provider.create.oauth2_settings.type
+      user_consent_description   = var.managed_identity_provider.create.oauth2_settings.user_consent_description
+      user_consent_display_name  = var.managed_identity_provider.create.oauth2_settings.user_consent_display_name
+      value                      = var.managed_identity_provider.create.oauth2_settings.role_value
+    }
+  }
+
+  web {
+    redirect_uris = ["https://${var.name}.azurewebsites.net/.auth/login/aad/callback"]
+
+    implicit_grant {
+      access_token_issuance_enabled = false
+      id_token_issuance_enabled     = true
+    }
+  }
+
+  required_resource_access {
+    resource_app_id = "00000003-0000-0000-c000-000000000000" # Microsoft Graph
+
+    resource_access {
+      id   = "e1fe6dd8-ba31-4d61-89e7-88639da4683d" # User.Read
+      type = "Scope"
+    }
+  }
+}
+
+resource "azuread_application_password" "password" {
+  count                 = local.should_create_app ? 1 : 0
+  application_object_id = azuread_application.application[0].object_id
+}
+
+resource "random_uuid" "oath2_uuid" {}
+
+# VNet integration
+
+resource "azurerm_app_service_virtual_network_swift_connection" "vnet_integration" {
+  count          = var.integration_subnet_id == null ? 0 : 1
+  app_service_id = azurerm_linux_function_app.function_app.id
+  subnet_id      = var.integration_subnet_id
+}
+
+# Logging and analytics
+
+data "azurerm_monitor_diagnostic_categories" "diagnostic_categories" {
+  count       = var.log_analytics_workspace_id == null ? 0 : 1
+  resource_id = azurerm_linux_function_app.function_app.id
+}
+
+resource "azurerm_monitor_diagnostic_setting" "diagnostic_setting" {
+  count                      = var.log_analytics_workspace_id == null ? 0 : 1
+  name                       = "diag-${var.name}"
+  target_resource_id         = azurerm_linux_function_app.function_app.id
+  log_analytics_workspace_id = var.log_analytics_workspace_id
+
+  dynamic "log" {
+    for_each = data.azurerm_monitor_diagnostic_categories.diagnostic_categories[0].logs
+
+    content {
+      category = log.value
+      enabled  = true
+
+      retention_policy {
+        enabled = false
+      }
+    }
+  }
+
+  dynamic "metric" {
+    for_each = data.azurerm_monitor_diagnostic_categories.diagnostic_categories[0].metrics
+
+    content {
+      category = metric.value
+      enabled  = true
+
+      retention_policy {
+        enabled = false
+      }
+    }
+  }
+}
---
+++ modules/azure/function_app_linux_managed_identity/variables.tf
@@ -0,0 +1,125 @@
+variable "location" {
+  type        = string
+  description = "A datacenter location in Azure."
+}
+
+variable "resource_group_name" {
+  type        = string
+  description = "Name of the resource group."
+}
+
+variable "name" {
+  type        = string
+  description = "Specifies the name of the function app."
+}
+
+variable "managed_identity_provider" {
+  type = object({
+    existing = optional(object({
+      client_id     = string
+      client_secret = string
+    }))
+    create = optional(object({
+      application_name = string
+      identifies_uris  = optional(list(string)) # if not set will default to api://<application_name>
+      display_name     = string
+      oauth2_settings = object({
+        admin_consent_description  = string
+        admin_consent_display_name = string
+        enabled                    = bool
+        type                       = string
+        user_consent_description   = string
+        user_consent_display_name  = string
+        role_value                 = string
+      })
+      owners        = optional(list(string)) # Deployment user will be added as owner by default
+      redirect_uris = optional(list(string)) # Only for additional URIs, function uri will be added by default
+    }))
+    allowed_clients = list(string)
+  })
+  validation {
+    condition     = var.managed_identity_provider.existing != null || var.managed_identity_provider.create != null
+    error_message = "Variable managed_identity_provider has to provide either an existing managed identity provider or given information to create one"
+  }
+  description = "The managed identity provider to use for connections on this function app"
+}
+
+variable "app_service_plan_id" {
+  type        = string
+  description = "The ID of the App Service Plan within which to create this Function App."
+}
+
+variable "log_analytics_workspace_id" {
+  type        = string
+  description = "ID of a log analytics workspace (optional)."
+  default     = null
+}
+
+variable "storage_account_name" {
+  type        = string
+  description = "The backend storage account name which will be used by this Function App (such as the dashboard, logs)."
+}
+
+variable "storage_account_access_key" {
+  type        = string
+  description = " The access key which will be used to access the backend storage account for the Function App."
+}
+
+variable "app_settings" {
+  type        = map(any)
+  description = "The app settings of this app service."
+  default     = {}
+}
+
+variable "runtime_version" {
+  type        = string
+  description = "The runtime version associated with the Function App."
+  default     = "~3"
+}
+
+variable "always_on" {
+  type        = bool
+  description = "Should the Function App be loaded at all times?"
+  default     = false
+}
+
+variable "connection_strings" {
+  type = list(object({
+    name  = string
+    type  = string
+    value = string
+  }))
+  description = "Connection strings for the function app."
+  default     = []
+}
+
+variable "integration_subnet_id" {
+  type        = string
+  description = "Define the subnet in which the Function App runs."
+  default     = null
+}
+
+variable "route_all_outbound_traffic" {
+  type        = bool
+  description = "Defines whether all outbound traffic of the Function App is routed."
+  default     = false
+}
+
+variable "ip_restriction" {
+  type = list(object({
+    ip_address                = string,
+    service_tag               = string,
+    virtual_network_subnet_id = string,
+    name                      = string,
+    priority                  = number,
+    action                    = string,
+    headers = list(object({
+      x_azure_fdid      = list(string),
+      x_fd_health_probe = list(string),
+      x_forwarded_for   = list(string),
+      x_forwarded_host  = list(string)
+    }))
+  }))
+  description = "A List of objects representing ip restrictions."
+  default     = null
+}
