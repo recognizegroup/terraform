a7b09f14a8f5699341d17a6302e03c4af967b8cb
Gerke Stam <g.stam@recognize.nl>
2022-11-15 10:49:16 +0100

    feedback PR

--- modules/azure/logic_app_trigger_http_request_data/main.tf
+++ modules/azure/logic_app_trigger_http_request_data/main.tf
@@ -3,7 +3,7 @@ terraform {
 
   required_providers {
     azapi = {
-      source = "azure/azapi"
+      source  = "azure/azapi"
       version = "1.0.0"
     }
     azurerm = "=3.10.0"
@@ -21,8 +21,8 @@ provider "azurerm" {
 
 // See also: https://github.com/hashicorp/terraform-provider-azurerm/issues/18866
 data "azapi_resource_action" "logicapp_callbackurl" {
-  resource_id = "${var.logic_app_id}/triggers/${var.trigger_name}"
-  action = "listCallbackUrl"
-  type = "Microsoft.Logic/workflows/triggers@2018-07-01-preview"
+  resource_id            = "${var.logic_app_id}/triggers/${var.trigger_name}"
+  action                 = "listCallbackUrl"
+  type                   = "Microsoft.Logic/workflows/triggers@2018-07-01-preview"
   response_export_values = ["value"]
 }
