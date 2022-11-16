ddc7a062dd87b8753549a73acb9510235abebfe0
Bart Wesselink <b.wesselink@recognize.nl>
2022-11-16 13:15:58 +0100

    WRKSPC-19 - Remove unused property

--- modules/azure/storage_account_private/main.tf
+++ modules/azure/storage_account_private/main.tf
@@ -35,7 +35,6 @@ resource "azurerm_private_endpoint" "private_endpoint" {
   location            = var.location
   resource_group_name = var.resource_group_name
   subnet_id           = var.subnet_id
-  private_dns_zone_id = var.private_dns_zone_id
 
   private_service_connection {
     name                           = "psc-${var.name}"
