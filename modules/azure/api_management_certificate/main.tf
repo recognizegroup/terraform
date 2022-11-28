5a261b38db612f1ad572879e57fe44fc66ed0f24
ArtiomMatiom <89966532+ArtiomMatiom@users.noreply.github.com>
2022-11-28 14:43:20 +0100

    Feature/11453 add certificate to apim (#239)
    * apim certificate module
    * key vault certificate module
    * Auto reniew Added
    
    Co-authored-by: ArtiomMatiom <Artiom.Matievschi@gmail.com>

---
+++ modules/azure/api_management_certificate/main.tf
@@ -0,0 +1,36 @@
+terraform {
+  required_version = ">=1.3.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.6.0"
+    }
+  }
+
+  backend "azurerm" {}
+}
+
+provider "azurerm" {
+  features {}
+}
+
+locals {
+}
+
+resource "azurerm_api_management_certificate" "apim_certificate" {
+  name                = var.name
+  api_management_name = var.api_management_name
+  resource_group_name = var.resource_group_name
+
+  key_vault_secret_id = var.keyvault_certificate_id
+  data                = var.certificate_location != null? filebase64(var.certificate_location): null
+  password            = var.certificate_password
+
+  lifecycle {
+    precondition {
+      condition     = (var.certificate_location != null && var.keyvault_certificate_id==null) || (var.certificate_location == null && var.keyvault_certificate_id!=null)
+      error_message = "Please, specify a Certificate KeyVault ID or Certificate file location"
+    }
+  }
+}
---
+++ modules/azure/key_vault_cetificate/variables.tf
@@ -0,0 +1,33 @@
+variable "name" {
+  type        = string
+  description = "Name of the certificate"
+}
+
+variable "key_vault_id" {
+  type        = string
+  description = "Key vault ID"
+}
+
+variable "key_properties" {
+  type = object({
+    key_size = number,
+    key_type = string
+  })
+  description = "key properties for certificate_policy of Certificate"
+
+  default = {
+    key_size = "4096"
+    key_type = "RSA"
+  }
+}
+
+variable "dns_names"{
+  type = list(string)
+  description = "List of dns names for the certificate"
+  default = []
+}
+
+variable "subject" {
+  type = string
+  description = "Certificate Subject"
+} 
