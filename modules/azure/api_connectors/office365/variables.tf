c25558a19f4f0f09b20e97bbc93e6d321b2f6293
ArtiomMatiom <Artiom.Matievschi@gmail.com>
2022-12-01 14:24:54 +0100

    add office 365 connection

---
+++ modules/azure/api_connectors/office365/variables.tf
@@ -0,0 +1,9 @@
+variable "resource_group_name" {
+  type        = string
+  description = "Resource group this connection should be placed in"
+}
+
+variable "connection_name" {
+  type        = string
+  description = "The name the connection should have"
+}
