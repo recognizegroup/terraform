f44ea2387d09024de94c74cc0ae63b5c26b3347e
ArtiomMatiom <89966532+ArtiomMatiom@users.noreply.github.com>
2022-12-01 14:30:10 +0100

    add office 365 connection (#241)
    * add office 365 connection
    
    * format
    
    Co-authored-by: ArtiomMatiom <Artiom.Matievschi@gmail.com>

---
+++ modules/azure/api_connectors/office365/main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_version = ">=1.0.9"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.26.0"
+    }
+  }
+
+  backend "azurerm" {}
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group_template_deployment" "log_analytics" {
+  name                = "${var.connection_name}_deployment"
+  resource_group_name = var.resource_group_name
+
+  template_content = file("./connection.json")
+  parameters_content = jsonencode({
+    "connection_name" = {
+      value = var.connection_name
+    }
+  })
+  deployment_mode = "Incremental"
+}
