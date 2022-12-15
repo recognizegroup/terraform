9fccdb14dc67aa8e86890474ac25d9d5310b7b5d
ArtiomMatiom <Artiom.Matievschi@gmail.com>
2022-12-15 15:44:07 +0100

    Add Add_output

--- modules/azure/function_app_linux_managed_identity/outputs.tf
+++ modules/azure/function_app_linux_managed_identity/outputs.tf
@@ -9,3 +9,7 @@ output "principal_id" {
 output "id" {
   value = azurerm_linux_function_app.function_app.id
 }
+
+output "aad_application_id" {
+  value ="${var.managed_identity_provider.existing == null ? azuread_application.application[0].application_id : var.managed_identity_provider.existing.client_id}"
+}
