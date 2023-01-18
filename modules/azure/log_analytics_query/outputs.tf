a9b45d57222c8195386395e038252c6ddbc65e6a
ArtiomMatiom <Artiom.Matievschi@gmail.com>
2023-01-18 12:47:28 +0100

    Query Modules

---
+++ modules/azure/log_analytics_query/outputs.tf
@@ -0,0 +1,12 @@
+# output "id" {
+#   value = azurerm_log_analytics_workspace.workspace.id
+# }
+
+# output "name" {
+#   value = azurerm_log_analytics_workspace.workspace.name
+# }
+
+# output "workspace_id" {
+#   value = azurerm_log_analytics_workspace.workspace.workspace_id
+# }
+
