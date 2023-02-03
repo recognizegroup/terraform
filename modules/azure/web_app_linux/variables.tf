3b6d795d139fc61d59cd5afe86c1361c9b548e64
Tom Reinders <t.reinders@recognize.nl>
2023-02-03 15:21:44 +0100

    TD-534 Init for web_app_linux and web_app_windows

---
+++ modules/azure/web_app_linux/variables.tf
@@ -0,0 +1,110 @@
+variable "location" {
+  type        = string
+  description = "A datacenter location in Azure."
+}
+
+variable "resource_group_name" {
+  type        = string
+  description = "Name of the resource group."
+}
+
+variable "name" {
+  type        = string
+  description = "Name of the app service."
+}
+
+variable "app_service_plan_id" {
+  type        = string
+  description = "ID of the app service plan."
+}
+
+variable "scm_type" {
+  type        = string
+  description = "The type of Source Control enabled for this App Service."
+  default     = "VSTSRM"
+}
+
+variable "always_on" {
+  type        = bool
+  description = "Indicates whether the App Service should be always on."
+  default     = true
+}
+
+variable "app_settings" {
+  type        = map(any)
+  description = "The app settings of this app service."
+  default     = {}
+}
+
+variable "linux_fx_version" {
+  type        = string
+  description = "Linux app framework and version for the app service."
+  default     = null
+}
+
+variable "dotnet_framework_version" {
+  type        = string
+  description = "The .NET framework version of this app service."
+  default     = "v5.0"
+}
+
+variable "websockets_enabled" {
+  type        = bool
+  description = "Indicates whether websockets are enabled within this app service."
+  default     = true
+}
+
+variable "health_check_path" {
+  type        = string
+  description = "The health check path to be pinged by app service."
+  default     = null
+}
+
+variable "integration_subnet_id" {
+  type        = string
+  description = "The ID of the integration subnet to enable virtual network integration."
+  default     = null
+}
+
+variable "private_subnet_id" {
+  type        = string
+  description = "The ID of the subnet from which private IP addresses will be allocated for the Private Endpoint."
+  default     = null
+}
+
+variable "custom_domains" {
+  type        = list(any)
+  description = "Define custom domains attached to this app service."
+  default     = []
+}
+
+variable "log_analytics_workspace_id" {
+  type        = string
+  description = "ID of a log analytics workspace (optional)."
+  default     = null
+}
+
+variable "use_32_bit_worker_process" {
+  type        = bool
+  description = "Should the App Service run in 32 bit mode, rather than 64 bit mode?"
+  default     = false
+}
+
+variable "ftps_state" {
+  type        = string
+  description = "State of FTP / FTPS service for this App Service."
+  default     = "FtpsOnly"
+}
+
+variable "storage_mount" {
+  type = object({
+    name         = string,
+    type         = string,
+    account_name = string,
+    share_name   = string,
+    access_key   = string
+    mount_path   = string
+  })
+  description = "Storage container or share to be mount within app service instance(s). Note that container mounts are read-only."
+  default     = null
+}
---
+++ modules/azure/web_app_windows/main.tf
@@ -0,0 +1,132 @@
+terraform {
+  required_version = ">=1.1.5"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.40"
+    }
+  }
+
+  backend "azurerm" {}
+}
+
+provider "azurerm" {
+  features {}
+}
+
+# TODO: Deprecated https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/guides/3.0-upgrade-guide#resource-azurerm_app_service
+resource "azurerm_app_service" "app_service" {
+  name                = var.name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  app_service_plan_id = var.app_service_plan_id
+  https_only          = true
+
+  site_config {
+    scm_type                  = var.scm_type
+    always_on                 = var.always_on
+    dotnet_framework_version  = var.dotnet_framework_version
+    websockets_enabled        = var.websockets_enabled
+    linux_fx_version          = var.linux_fx_version
+    health_check_path         = var.health_check_path
+    use_32_bit_worker_process = var.use_32_bit_worker_process
+    ftps_state                = var.ftps_state
+    http2_enabled             = true
+    min_tls_version           = 1.2
+  }
+
+  app_settings = var.app_settings
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  // Configures a mountpoint to a storage container (ro) or file share (rw)
+  // Read more: https://docs.microsoft.com/en-us/azure/app-service/configure-connect-to-azure-storage
+  dynamic "storage_account" {
+    for_each = var.storage_mount == null ? [] : [1]
+    content {
+      name         = var.storage_mount.name
+      type         = var.storage_mount.type
+      account_name = var.storage_mount.account_name
+      access_key   = var.storage_mount.access_key
+      share_name   = var.storage_mount.share_name
+      mount_path   = var.storage_mount.mount_path
+    }
+  }
+}
+
+resource "azurerm_app_service_virtual_network_swift_connection" "vnet_integration" {
+  count          = var.integration_subnet_id == null ? 0 : 1
+  app_service_id = azurerm_app_service.app_service.id
+  subnet_id      = var.integration_subnet_id
+}
+
+resource "azurerm_private_endpoint" "private_endpoint" {
+  count               = var.private_subnet_id == null ? 0 : 1
+  name                = "pe-${var.name}"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  subnet_id           = var.private_subnet_id
+
+  private_service_connection {
+    name                           = "psc-${var.name}"
+    is_manual_connection           = false
+    private_connection_resource_id = azurerm_app_service.app_service.id
+    subresource_names              = ["sites"]
+  }
+
+  # Should be deployed by Azure policy
+  lifecycle {
+    ignore_changes = [private_dns_zone_group]
+  }
+}
+
+resource "azurerm_app_service_custom_hostname_binding" "custom_domain" {
+  for_each            = toset(var.custom_domains)
+  hostname            = each.value
+  app_service_name    = azurerm_app_service.app_service.name
+  resource_group_name = var.resource_group_name
+}
+
+data "azurerm_monitor_diagnostic_categories" "diagnostic_categories" {
+  count       = var.log_analytics_workspace_id == null ? 0 : 1
+  resource_id = azurerm_app_service.app_service.id
+}
+
+resource "azurerm_monitor_diagnostic_setting" "diagnostic_setting" {
+  count                      = var.log_analytics_workspace_id == null ? 0 : 1
+  name                       = "diag-${var.name}"
+  target_resource_id         = azurerm_app_service.app_service.id
+  log_analytics_workspace_id = var.log_analytics_workspace_id
+
+  // TODO: not yet implemented by Azure
+  // log_analytics_destination_type = "Dedicated"
+
+  dynamic "log" {
+    for_each = data.azurerm_monitor_diagnostic_categories.diagnostic_categories[0].logs
+
+    content {
+      category = log.value
+      enabled  = true
+
+      retention_policy {
+        enabled = false
+      }
+    }
+  }
+
+  dynamic "metric" {
+    for_each = data.azurerm_monitor_diagnostic_categories.diagnostic_categories[0].metrics
+
+    content {
+      category = metric.value
+      enabled  = true
+
+      retention_policy {
+        enabled = false
+      }
+    }
+  }
+}
