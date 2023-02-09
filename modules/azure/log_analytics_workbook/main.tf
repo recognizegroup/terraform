15e26b0be60ff5468f09ebf08a788fe17d72282b
ArtiomMatiom <a.matievschi@recognize.nl>
2023-02-09 16:48:37 +0100

    First iteration, Add FIle reading create random guid

--- modules/azure/log_analytics_workbook/main.tf
+++ modules/azure/log_analytics_workbook/main.tf
@@ -16,20 +16,53 @@ provider "azurerm" {
 }
 
 locals {
-  // Remove need for specifying the "value" field for every parameter
-  parameters_content = {
-    for key, value in var.arm_parameters :
-    key => { "value" = value }
-  }
+  # // Remove need for specifying the "value" field for every parameter
+  # parameters_content = {
+  #   for key, value in var.arm_parameters :
+  #   key => { "value" = value }
+  # }
 }
 
 // Deploy workbook as ARM template conditional when arm_template_path is specified
 // To export the ARM template from the Azure portal go to Log Analytics workspaces > Automation > Export Template
-resource "azurerm_resource_group_template_deployment" "workflow_deployment" {
-  count               = var.arm_template_path == null ? 0 : 1
-  name                = "${var.workbook_name}-deployment"
+# resource "azurerm_resource_group_template_deployment" "workflow_deployment" {
+#   count               = var.arm_template_path == null ? 0 : 1
+#   name                = "${var.workbook_name}-deployment"
+#   resource_group_name = var.resource_group_name
+#   deployment_mode     = "Incremental"
+#   template_content    = file(var.arm_template_path)
+#   parameters_content  = jsonencode(local.parameters_content)
+# }
+
+resource "random_uuid" "test" {
+}
+
+
+
+resource "azurerm_resource_group_template_deployment" "key_vault_managed_identity" {
+  name                = "${var.workbook_name}_deployment"
   resource_group_name = var.resource_group_name
-  deployment_mode     = "Incremental"
-  template_content    = file(var.arm_template_path)
-  parameters_content  = jsonencode(local.parameters_content)
+
+  template_content = file("./test.json")
+  parameters_content = jsonencode({
+    "workbook_name" = {
+      value = var.workbook_name
+    }
+
+    "workbookId" = {
+      value = random_uuid.test.result
+    }
+
+    "serializedData" = {
+      value = jsondecode(file(var.serializedDataFile))
+    }
+
+    "sourceId" = {
+      value = var.resource_id_with_data 
+    }
+    # "key_vault_name" = {
+    #   value = var.keyvault_name
+    # }
+  })
+  deployment_mode = "Incremental"
 }
--- modules/azure/log_analytics_workbook/variables.tf
+++ modules/azure/log_analytics_workbook/variables.tf
@@ -1,7 +1,7 @@
-variable "location" {
-  type        = string
-  description = "A datacenter location in Azure."
-}
+# variable "location" {
+#   type        = string
+#   description = "A datacenter location in Azure."
+# }
 
 variable "resource_group_name" {
   type        = string
@@ -13,14 +13,24 @@ variable "workbook_name" {
   description = "Specifies the name of the Log Analytics Workbook."
 }
 
-variable "arm_template_path" {
-  type        = string
-  description = "Specifies the path of ARM template to be used during deployment."
-  default     = null
+variable "serializedDataFile" {
+  type = string
+  description = "location of Serialized Data"
 }
 
-variable "arm_parameters" {
-  type        = any
-  description = "Parameter values for the ARM template."
-  default     = {}
+variable "resource_id_with_data" {
+  type =  string
+  description = "resource id that stores the data, such as Log analytics ID"
 }
+
+# variable "arm_template_path" {
+#   type        = string
+#   description = "Specifies the path of ARM template to be used during deployment."
+#   default     = null
+# }
+
+# variable "arm_parameters" {
+#   type        = any
+#   description = "Parameter values for the ARM template."
+#   default     = {}
+# }
