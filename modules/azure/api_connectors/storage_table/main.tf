7a483886eb684214020182b0ac7d307682457bd6
Michal Pipal <m.pipal@recognize.nl>
2023-03-08 08:51:25 +0100

    Update Azure API Connectors

---
+++ modules/azure/api_connectors/storage_table/main.tf
@@ -0,0 +1,35 @@
+terraform {
+  required_version = "~> 1.3"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.40"
+    }
+  }
+
+  backend "azurerm" {}
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group_template_deployment" "storage_account" {
+  name                = "${var.connection_name}_deployment"
+  resource_group_name = var.resource_group_name
+
+  template_content = file("./connection.json")
+  parameters_content = jsonencode({
+    "connection_name" = {
+      value = var.connection_name
+    }
+    "storage_account_name" = {
+      value = var.storage_account_name
+    }
+    "storage_account_access_key" = {
+      value = var.storage_account_access_key
+    }
+  })
+  deployment_mode = "Incremental"
+}
