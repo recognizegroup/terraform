5f63911d10b31fe48a43c21f3b4fe6c2cef1748e
Tom Reinders <t.reinders@recognize.nl>
2023-03-16 18:47:27 +0100

    TD-239 Make error terraform validation

--- validate/validate.sh
+++ validate/validate.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -e
+
 find ./modules -name "*.tf" -exec dirname {} + | sort | uniq | while read -r line ; do
   echo "Run test on $line"
   cp ./validate/main_override.tf.dist "$line/main_override.tf"
