1f4b56262a243c36c6f3ba4758a56d0845fe34f6
Tom Reinders <t.reinders@recognize.nl>
2023-03-16 19:27:56 +0100

    TD-239 Clean up splitter

--- validate/validate.sh
+++ validate/validate.sh
@@ -3,9 +3,7 @@
 set -e
 
 find ./modules -name "*.tf" -exec dirname {} + | sort | uniq | while read -r line ; do
-  echo ""
   echo "================================================================================================"
-  echo ""
   echo "Run test on $line"
   cp ./validate/main_override.tf.dist "$line/main_override.tf"
   cd "$line" || exit
