7b0c705e3c474b9324f7b64cdca73fe9bb433c78
Bart Wesselink <b.wesselink@recognize.nl>
2023-04-04 17:26:52 +0200

    TD-566 - Add performance dashboard for application

---
+++ modules/azure/application_performance_workbook/main.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_version = "~> 1.3"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.48"
+    }
+  }
+
+  backend "azurerm" {}
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "random_uuid" "workbook_guid" {
+}
+
+resource "azurerm_application_insights_workbook" "workbook" {
+  name                = random_uuid.workbook_guid.result
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  display_name        = var.workbook_name
+
+  // Trick that allows to take any json file and Minimize it
+  data_json = jsonencode(jsondecode(file("${path.module}/workbook.json")))
+  source_id = lower(var.log_analytics_id)
+}
---
+++ modules/azure/application_performance_workbook/variables.tf
@@ -0,0 +1,19 @@
+variable "location" {
+  type        = string
+  description = "A datacenter location in Azure."
+}
+
+variable "resource_group_name" {
+  type        = string
+  description = "Name of the resource group."
+}
+
+variable "workbook_name" {
+  type        = string
+  description = "Specifies the name of the Log Analytics Workbook."
+}
+
+variable "log_analytics_id" {
+  type        = string
+  description = "ID of the Log Analytics Workspace."
+}
