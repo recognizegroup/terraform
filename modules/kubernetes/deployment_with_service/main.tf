c8556d08cce692e8c43054d1bf97673625d15650
Bart Wesselink <b.wesselink@recognize.nl>
2023-04-21 09:28:22 +0200

    CM-27 - Support no scaler

--- modules/kubernetes/deployment_with_service/main.tf
+++ modules/kubernetes/deployment_with_service/main.tf
@@ -203,7 +203,7 @@ resource "kubernetes_manifest" "http-scaler" {
 
 #
 resource "kubernetes_service_v1" "http-scaler-service-proxy" {
-  count = var.scaler != null && var.scaler.type == "http" ? 1 : 0
+  count = var.scaler != null && lookup(var.scaler, "type", "-") == "http" ? 1 : 0
 
   metadata {
     name      = "${var.name}-keda-bridge"
@@ -227,7 +227,7 @@ resource "kubernetes_service_v1" "http-scaler-service-proxy" {
 }
 
 resource "kubernetes_horizontal_pod_autoscaler_v2" "resource-scaler" {
-  count = var.scaler != null && var.scaler.type == "resource" ? 1 : 0
+  count = var.scaler != null && lookup(var.scaler, "type", "-") == "resource" ? 1 : 0
 
   metadata {
     name      = var.name
