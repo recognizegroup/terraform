c9917fa92c38ac5619c119a55223f8c0e6d73278
PatrikKovacs <p.kovacs@recognize.nl>
2023-10-16 14:13:30 +0200

    Fixed PR comments

--- modules/azure/iam_set/main.tf
+++ modules/azure/iam_set/main.tf
@@ -23,13 +23,15 @@ locals {
         scope                = role.scope
         role_definition_name = role.role_name
         principal_id         = principal_id
+        role_name            = role.name
       }
     ]
   ])
 }
 
 resource "azurerm_role_assignment" "role_assignment" {
-  for_each = { for idx, ra in local.role_assignments : idx => ra }
+  for_each = { for ra in local.role_assignments :
+  "${ra.principal_id}_${ra.role_definition_name}${ra.role_name != null ? "_${ra.role_name}" : ""}" => ra }
 
   scope                = each.value.scope
   role_definition_name = each.value.role_definition_name
