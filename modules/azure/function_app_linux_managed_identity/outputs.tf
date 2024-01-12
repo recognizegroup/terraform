1a65f82cc3e9c557d9ac0be466c496cf1d5c1339
zjanura <zdenek.janura@isonsoft.cz>
2024-01-12 17:10:17 +0100

    linux function output audience

--- modules/azure/function_app_linux_managed_identity/outputs.tf
+++ modules/azure/function_app_linux_managed_identity/outputs.tf
@@ -13,3 +13,7 @@ output "id" {
 output "aad_application_name" {
   value = var.managed_identity_provider.existing == null ? "api://${var.managed_identity_provider.create.application_name}" : null
 }
+
+output "api_audience" {
+  value = tolist(tolist(tolist(data.azurerm_linux_function_app.function_app.auth_settings)[0].active_directory)[0].allowed_audiences)[0]
+}
