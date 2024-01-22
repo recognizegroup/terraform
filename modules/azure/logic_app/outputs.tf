49a6b4c53e5978fa1e8f5cfdea53c21d38732ad9
Michal Pipal <m.pipal@recognize.nl>
2024-01-22 14:26:57 +0100

    Fixed typo in the logic app output

--- modules/azure/logic_app/outputs.tf
+++ modules/azure/logic_app/outputs.tf
@@ -2,7 +2,7 @@ output "principal_id" {
   value = var.use_managed_identity ? azurerm_logic_app_workflow.workflow.identity[0].principal_id : null
 }
 
-output "enpoint" {
+output "endpoint" {
   value = azurerm_logic_app_workflow.workflow.access_endpoint
 }
 output "id" {
