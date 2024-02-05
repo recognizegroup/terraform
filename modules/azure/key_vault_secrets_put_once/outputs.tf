53368995cde2c9977122774787569d2b3dfb076c
Szymon Rozek <szymonrozek>
2024-02-05 12:25:18 +0100

    fmt

--- modules/azure/key_vault_secrets_put_once/outputs.tf
+++ modules/azure/key_vault_secrets_put_once/outputs.tf
@@ -2,8 +2,8 @@ output "secrets" {
   value = {
     for prop in values(resource.azurerm_key_vault_secret.secret)[*] :
     prop.name => {
-      value            = prop.value
-      versionlessid    = prop.versionless_id
+      value         = prop.value
+      versionlessid = prop.versionless_id
     }
   }
   sensitive = true
