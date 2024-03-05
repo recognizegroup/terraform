f533b090047a0fe2e48d1f4a645131f42f91822c
zjanura <zdenek.janura@isonsoft.cz>
2024-03-05 15:09:34 +0100

    new operation module

---
+++ modules/azure/api_management_api_operation/main.tf
@@ -0,0 +1,40 @@
+terraform {
+  required_version = "~> 1.3"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.48"
+    }
+  }
+
+  backend "azurerm" {}
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_api_management_api_operation" "operation" {
+  operation_id        = var.operation_id
+  api_name            = var.api_name
+  api_management_name = var.api_management_name
+  resource_group_name = var.resource_group_name
+  display_name        = var.display_name
+  method              = var.operation_method
+  url_template        = var.url_template
+  description         = var.description
+
+  response {
+    status_code = 200
+  }
+}
+
+resource "azurerm_api_management_api_operation_policy" "policy" {
+  api_name            = var.api_name
+  api_management_name = var.api_management_name
+  resource_group_name = var.resource_group_name
+
+  operation_id = var.operation_id
+  xml_content  = var.policy.file_location != null ? file(var.policy.file_location) : var.policy.xml_string
+}
---
+++ modules/azure/api_management_api_operation/variables.tf
@@ -0,0 +1,51 @@
+variable "resource_group_name" {
+  type        = string
+  description = "Name of the resource group."
+}
+
+variable "operation_id" {
+  type        = string
+  description = "id of the operation"
+}
+
+variable "api_management_name" {
+  type        = string
+  description = "The name of the API management service."
+}
+
+variable "api_name" {
+  type        = string
+  description = "API name"
+}
+
+variable "display_name" {
+  type        = string
+  description = "Operation display name"
+}
+
+variable "operation_method" {
+  type        = string
+  description = "Operation method"
+}
+
+variable "url_template" {
+  type        = string
+  description = "Url of operation"
+}
+
+variable "description" {
+  type        = string
+  description = "Description of operation"
+}
+
+variable "policy" {
+  type = object({
+    file_location = optional(string),
+    xml_string    = optional(string)
+  })
+  validation {
+    condition     = (var.policy.file_location != null || var.policy.xml_string != null)
+    error_message = "Either file_location or xml_string must be provided."
+  }
+  description = "XML policy described by a file (location) or raw string."
+}
