5ac321e29c7e37f780933d22082b4c784389db5a
zjanura <zdenek.janura@isonsoft.cz>
2024-03-05 15:20:32 +0100

    output varibles adjusted

--- modules/azure/api_management_api_operation/outputs.tf
+++ modules/azure/api_management_api_operation/outputs.tf
@@ -1,3 +1,7 @@
-output "id" {
+output "policy_id" {
   value = azurerm_api_management_api_operation_policy.policy.id
+}
+
+output "operation_id" {
+  value = azurerm_api_management_api_operation.id
 }
