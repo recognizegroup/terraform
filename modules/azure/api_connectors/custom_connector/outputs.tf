dd9e12f5b24eb0c0626de95ba7a6865b5a81c8c9
ArtiomMatiom <a.matievschi@recognize.nl>
2024-03-06 11:15:20 +0100

    auto format

--- modules/azure/api_connectors/custom_connector/outputs.tf
+++ modules/azure/api_connectors/custom_connector/outputs.tf
@@ -2,6 +2,6 @@ output "name" {
   value = var.connection_name
 }
 
-output "connector_id"{
+output "connector_id" {
   value = "${data.azurerm_subscription.current.id}/resourceGroups/${var.connector_resource_group_name}/providers/Microsoft.Web/customApis/${var.connector_name}"
 }
