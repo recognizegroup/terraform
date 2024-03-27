704075f6987eb00e616a23de955951a05357ca02
Szymon Rozek <szymonrozek>
2024-03-27 13:48:37 +0100

    push fix

--- modules/azure/function_app_linux_managed_identity/main.tf
+++ modules/azure/function_app_linux_managed_identity/main.tf
@@ -257,17 +257,20 @@ resource "azurerm_monitor_diagnostic_setting" "diagnostic_setting" {
   target_resource_id         = azurerm_linux_function_app.function_app.id
   log_analytics_workspace_id = var.log_analytics_workspace_id
 
-  dynamic "enabled_log" {
-    for_each = data.azurerm_monitor_diagnostic_categories.diagnostic_categories[0].log_category_types
-
-    content {
-      category = enabled_log.value
-
-      retention_policy {
-        enabled = false
-      }
-    }
-  }
+ # dynamic "enabled_log" {
+ #   for_each = data.azurerm_monitor_diagnostic_categories.diagnostic_categories[0].log_category_types
+ #
+ #
+ #   content {
+ #     category = enabled_log.value
+ #
+ #
+ # 
+ #     retention_policy {
+ #       enabled = false
+ #     }
+ #   }
+ # }
 
   dynamic "metric" {
     for_each = data.azurerm_monitor_diagnostic_categories.diagnostic_categories[0].metrics
