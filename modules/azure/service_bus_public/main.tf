ccca0c9cb5d31fa727ab0d8f31a410e6239b496c
Michal Pipal <73311540+pipalmic@users.noreply.github.com>
2024-06-19 17:09:16 +0200

    Added diagnostic settings for ServiceBus module AB#20426 (#413)

--- modules/azure/service_bus_public/main.tf
+++ modules/azure/service_bus_public/main.tf
@@ -31,3 +31,32 @@ resource "azurerm_servicebus_namespace_authorization_rule" "authorization_rule" 
   send   = var.authorization_rule.send
   manage = var.authorization_rule.manage
 }
+
+data "azurerm_monitor_diagnostic_categories" "diagnostic_categories" {
+  count       = var.log_analytics_workspace_id == null ? 0 : 1
+  resource_id = azurerm_servicebus_namespace.service_bus.id
+}
+
+resource "azurerm_monitor_diagnostic_setting" "diagnostic_setting" {
+  count                      = var.log_analytics_workspace_id == null ? 0 : 1
+  name                       = "diag-${var.name}"
+  target_resource_id         = azurerm_servicebus_namespace.service_bus.id
+  log_analytics_workspace_id = var.log_analytics_workspace_id
+
+  dynamic "enabled_log" {
+    for_each = data.azurerm_monitor_diagnostic_categories.diagnostic_categories[0].log_category_types
+
+    content {
+      category = enabled_log.value
+    }
+  }
+
+  dynamic "metric" {
+    for_each = data.azurerm_monitor_diagnostic_categories.diagnostic_categories[0].metrics
+
+    content {
+      category = metric.value
+      enabled  = true
+    }
+  }
+}
