recognizegroup/terraform

commit 60a1caa7dec33ba00ae0806d858763b171b2887d

Storage account soft delete setting AB#20123 (#411)
* Adding soft delete setting for storage account

* fix cors rules

* fix formating

modules/azure/storage_account_public/main.tf
@@ -28,20 +28,24 @@ resource "azurerm_storage_account" "storage_account" {
   nfsv3_enabled                   = var.nfsv3_enabled
   is_hns_enabled                  = var.is_hns_enabled
 
-  dynamic "blob_properties" {
-    for_each = var.cors_rules != null ? [1] : []
+  blob_properties {
+    dynamic "cors_rule" {
+      for_each = var.cors_rules != null ? [1] : []
 
-    content {
-      dynamic "cors_rule" {
-        for_each = var.cors_rules
+      content {
+        allowed_headers    = var.cors_rules.value.allowed_headers
+        allowed_methods    = var.cors_rules.value.allowed_methods
+        allowed_origins    = var.cors_rules.value.allowed_origins
+        exposed_headers    = var.cors_rules.value.exposed_headers
+        max_age_in_seconds = var.cors_rules.value.max_age_in_seconds
+      }
+    }
 
-        content {
-          allowed_headers    = cors_rule.value.allowed_headers
-          allowed_methods    = cors_rule.value.allowed_methods
-          allowed_origins    = cors_rule.value.allowed_origins
-          exposed_headers    = cors_rule.value.exposed_headers
-          max_age_in_seconds = cors_rule.value.max_age_in_seconds
-        }
+    dynamic "delete_retention_policy" {
+      for_each = var.delete_retention_policy_days != null ? [1] : []
+
+      content {
+        days = var.delete_retention_policy_days
       }
     }
   }

modules/azure/storage_account_public/variables.tf
@@ -109,3 +109,9 @@ variable "custom_domain" {
   description = "Describes the custom domain configuration for the storage account"
   default     = null
 }
+
+variable "delete_retention_policy_days" {
+  type        = number
+  description = "Number of days to retain deleted blobs."
+  default     = null
+}

commit ac8f24f9aef3d45fe3166d5c572181b1420ede6c

Fix bug introduced in #411 that breaks the cors rules from running

modules/azure/storage_account_public/main.tf
@@ -30,14 +30,14 @@ resource "azurerm_storage_account" "storage_account" {
 
   blob_properties {
     dynamic "cors_rule" {
-      for_each = var.cors_rules != null ? [1] : []
+      for_each = var.cors_rules
 
       content {
-        allowed_headers    = var.cors_rules.value.allowed_headers
-        allowed_methods    = var.cors_rules.value.allowed_methods
-        allowed_origins    = var.cors_rules.value.allowed_origins
-        exposed_headers    = var.cors_rules.value.exposed_headers
-        max_age_in_seconds = var.cors_rules.value.max_age_in_seconds
+        allowed_headers    = cors_rule.value.allowed_headers
+        allowed_methods    = cors_rule.value.allowed_methods
+        allowed_origins    = cors_rule.value.allowed_origins
+        exposed_headers    = cors_rule.value.exposed_headers
+        max_age_in_seconds = cors_rule.value.max_age_in_seconds
       }
     }
 

modules/azure/storage_account_public/variables.tf
@@ -89,7 +89,7 @@ variable "cors_rules" {
     max_age_in_seconds = number
   }))
   description = "Describes the CORS rules for the storage account"
-  default     = null
+  default     = []
 }
 
 variable "static_website" {