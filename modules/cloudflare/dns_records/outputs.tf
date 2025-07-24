cdbf792d5b724e2f73cfc47ebfb26f98773ddf3d
Nikola Petrovic <nikola.petrovic@valcon.com>
2025-07-24 10:19:32 +0200

    CF: fixed outputs

--- modules/cloudflare/dns_records/outputs.tf
+++ modules/cloudflare/dns_records/outputs.tf
@@ -1,7 +1,7 @@
 output "created_on" {
-  value = { for k, v in data.cloudflare_record.record : k => v.created_on }
+  value = { for k, v in cloudflare_record.record : k => v.created_on }
 }
 
 output "modified_on" {
-  value = { for k, v in data.cloudflare_record.record : k => v.modified_on }
+  value = { for k, v in cloudflare_record.record : k => v.modified_on }
 }
